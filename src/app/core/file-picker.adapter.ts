6204dfa62f9beaef2bc0179c3ad741c98c97790c
root <root@iPhone>
2021-10-21 10:23:41 -0400

    imgCssca

--- src/app/core/file-picker.adapter.ts
+++ src/app/core/file-picker.adapter.ts
@@ -24,7 +24,7 @@ export class DemoFilePickerAdapter extends FilePickerAdapter {
   public uploadFile(fileItem: FilePreviewModel) {
     const form = new FormData();
     form.append('file', fileItem.file);
-    const api = 'https://db.penguinscleaning.ca:3017/api/containers/tixsImages/upload';
+    const api = 'https://db.buckapi.com:3035/api/containers/tixsImages/upload';
     const req = new HttpRequest('POST', api, form, {reportProgress: true});
     return this.http.request(req)
     .pipe(
@@ -32,7 +32,7 @@ export class DemoFilePickerAdapter extends FilePickerAdapter {
           if (res.type === HttpEventType.Response) {
          this._uw.file=res.body.result.files.file;
          // console.log("Nombre: ",this._uw.file[0].name);
-         this._uw.images.push('https://db.penguinscleaning.ca:80/imgPenguins/server/local-storage/tixsImages/'+this._uw.file[0].name);
+         this._uw.images.push('https://db.buckapi.ca:80/imgPenguins/server/local-storage/tixsImages/'+this._uw.file[0].name);
           return res.body.id.toString();
        
         } else if (res.type ===  HttpEventType.UploadProgress) {
@@ -44,7 +44,7 @@ export class DemoFilePickerAdapter extends FilePickerAdapter {
       );
   }
     public removeFile(fileItem): Observable<any> {
-    const removeApi = 'https://db.penguinscleaning.ca/api/containers/tixsImages/upload';
+    const removeApi = 'https://db.buckapi.com/api/containers/tixsImages/upload';
     return this.http.post(removeApi, {});
     }
 }
